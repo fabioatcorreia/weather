44882b66025f94876cc3d6596cdd1647eb7e6c3e
Fábio Correia <fatcorreia94@gmail.com>
2018-12-21 01:56:45 +0000

    chore: added config to cache responses with service worker

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          // To match cross-origin requests, use a RegExp that matches
+          // the start of the origin:
+          urlPattern: new RegExp(
+            "^https://query.yahooapis.com/v1/public/yql(.*)"
+          ),
+          handler: "cacheFirst",
+          options: {
+            cacheName: "yahoo-weather",
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
+    }
+  }
+};
